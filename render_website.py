e9fa4fb69d66b4d3e71a11568b89a41b3e67a8fc
Aw1ks <aw1ksss331@gmail.com>
2025-06-19 20:06:08 +0300

    added argparse for user to specify folders to use

--- render_website.py
+++ render_website.py
@@ -3,6 +3,7 @@ import more_itertools
 import os
 import shutil
 import math
+import argparse
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
@@ -14,10 +15,30 @@ def load_json(file_name):
         received_json_file = json.load(my_file)
     return received_json_file
 
+
 def main():
-    folder = 'pages'
-    shutil.rmtree(folder, ignore_errors=True)
-    os.makedirs(folder, exist_ok=True)
+    folder_for_pages = 'pages'
+    folder_download_info = 'meta_data.json'
+
+    parser = argparse.ArgumentParser(
+        prog='Online library',
+        description='''This project receives data from the specified JSON file with books, 
+        creates website pages using an HTML template, and starts the server.''')
+    
+    parser.add_argument('--info_folder',
+                        type=str,
+                        default=folder_download_info,
+                        help='Path to the folder for downloading book information')
+    
+    parser.add_argument('--pages_folder',
+                        type=str,
+                        default=folder_for_pages,
+                        help='Path to the folder for downloading pages')
+    
+    args = parser.parse_args()
+
+    shutil.rmtree(folder_for_pages, ignore_errors=True)
+    os.makedirs(folder_for_pages, exist_ok=True)
 
     env = Environment(
         loader=FileSystemLoader('.'),
@@ -25,25 +46,21 @@ def main():
     )
     template = env.get_template('template.html')
 
-    meta_data = load_json("meta_data.json")
+    meta_data = load_json(args.info_folder)
 
-    if meta_data:
-        chunked_meta_data = more_itertools.chunked(meta_data, 10)
-        number_pages = math.ceil(len(meta_data) / 10)
-        
-        for num_page, book  in enumerate(chunked_meta_data, 1):
-            rendered_page = template.render(
-                books=book,
-                current_page=num_page,
-                number_pages=number_pages,
-            )
+    chunked_meta_data = more_itertools.chunked(meta_data, 10)
+    number_pages = math.ceil(len(meta_data) / 10)
+    
+    for num_page, book  in enumerate(chunked_meta_data, 1):
+        rendered_page = template.render(
+            books=book,
+            current_page=num_page,
+            number_pages=number_pages,
+        )
 
-            filename = f"pages/index{num_page}.html"
-            with open(filename, 'w', encoding='utf8') as file:
-                file.write(rendered_page)
-
-    else:
-        print('Файл meta_data.json пуст.')
+        filename = f"pages/index{num_page}.html"
+        with open(filename, 'w', encoding='utf8') as file:
+            file.write(rendered_page)
 
     server = Server()
     server.watch('template.html', main)
